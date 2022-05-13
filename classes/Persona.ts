a077bec255bd3a8e2a34daeacef37d2d8bbd1957
unknown <usuario@PUERTA18.CORP.AR>
2022-05-13 12:20:10 -0300

    editando el .gitignore

---
+++ classes/Persona.ts
@@ -0,0 +1,39 @@
+export class Persona {
+    private nacimiento: number ;
+    private nombre: string ;
+
+
+    constructor(anioNacimiento: number, nombre: string ){
+        this.nacimiento = anioNacimiento ;
+        this.nombre = nombre ;
+        
+    };
+    
+    public cualEsTuNombre(): string{
+        return "Mi nombre es "+ this.nombre.split(" ");
+    };
+
+    public cualEsTuEdad(): string {
+        let edad: number = 2022 - this.nacimiento; 
+        return "Mi edad es " + edad + " años" ;
+
+    };
+
+    public cualEsTuGeneracion(){
+        if (this.nacimiento < 1975) {
+            return "Soy un Baby Boomer";
+
+        }
+        if (this.nacimiento >= 1975 && this.nacimiento < 1999) {
+            return "Soy un Millenial" ;
+        }
+        if (this.nacimiento >= 1999 && this.nacimiento < 2010) {
+            return "Soy un Centenial" ;
+
+        }
+        if (this.nacimiento >= 2010 ){
+            return "Soy un Alfa" ;
+        }
+    };
+};
+
---
+++ index.ts
@@ -0,0 +1,4 @@
+let fede = new Persona(2004, "FedericoFernandez");
+console.log(fede.cualEsTuNombre());
+console.log(fede.cualEsTuGeneracion());
+console.log(fede.cualEsTuEdad());
